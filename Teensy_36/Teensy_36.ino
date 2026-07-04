30cb96fc4745ab3047ebc14e2ebff8fb35638118
leonarddelor <leonarddelor3@gmail.com>
2026-07-04 15:29:40 +0200

    Phase 1 (premier test)
    J'ai ajouté un code de manière à ce que la teensy puisse lire un fichier audio depuis une carte sd sur reaper.
    Finalement je n'ai eu aucun son sur reaper.

--- Teensy_36/Teensy_36.ino
+++ Teensy_36/Teensy_36.ino
@@ -1,8 +1,42 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// Création des objets Audio
+AudioPlaySdWav           playWav1;       
+AudioOutputUSB           audioOutput;    // Envoie l'audio vers le PC via l'USB
+
+// Connexion du lecteur SD vers la sortie USB (Canal Gauche et Droite)
+AudioConnection          patchCord1(playWav1, 0, audioOutput, 0);
+AudioConnection          patchCord2(playWav1, 1, audioOutput, 1);
+
+// Utiliser le lecteur de carte SD intégré de la Teensy 3.6
+#define SDCARD_CS_PIN    BUILTIN_SDCARD
+
 void setup() {
-  // Initialise la broche de la LED comme une sortie
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  Serial.begin(9600);
+  
+  // Allouer de la mémoire pour le traitement audio
+  AudioMemory(16);
+
+  // Initialisation de la carte SD intégrée
+  if (!(SD.begin(SDCARD_CS_PIN))) {
+    while (1) {
+      Serial.println("Erreur : Impossible de lire la carte SD !");
+      delay(500);
+    }
+  }
+  
+  delay(1000);
 }
 
 void loop() {
+  if (playWav1.isPlaying() == false) {
+    Serial.println("Lecture du fichier...");
+    // Remplace "TRACK1.WAV" par le nom exact de ton fichier sur la SD
+    playWav1.play("TRACK1.WAV"); 
+    delay(10); // Court délai pour laisser le lecteur s'initialiser
+  }
 }
