18f31f2e72b70c7abae51270e87e033b5df3d742
leonarddelor <leonarddelor3@gmail.com>
2026-07-05 13:51:32 +0200

    Phase 1 (troisième test)
    j'ai utilisé l'audio library pour créer le flux audio en récupérant ce qui venait de la carte sd en sortant par le port usb. finalement ça fait la même chose que ce que j'avait codé mais avec des nomenclatures légèrement différentes. Pour le test avec le i2s ça va devoir attende à ce que j'ai acces à d'autres composants pour ajouter une sortie physique, puisque le i2s transforme le signal numérique en signal électrique, permettant de sortir par une prise jack par exemple. ce qui me permettra de récupérer le son de l'ordi et le sortir dans des enceintes. mais apparament j'aurai besoin d'un dac pour que ça doit possible.

--- Teensy_36/Teensy_36.ino
+++ Teensy_36/Teensy_36.ino
@@ -4,15 +4,14 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
-// Création des objets Audio
-AudioPlaySdWav           playWav1;       
-AudioOutputUSB           audioOutput;    // Envoie l'audio vers le PC via l'USB
+// GUItool: begin automatically generated code
+AudioPlaySdWav           playSdWav1;     //xy=242.1999969482422,169.1999969482422
+AudioOutputUSB           usb1;           //xy=497.20003509521484,170.19999885559082
+AudioConnection          patchCord1(playSdWav1, 0, usb1, 0);
+AudioConnection          patchCord2(playSdWav1, 1, usb1, 1);
+// GUItool: end automatically generated code
 
-// Connexion du lecteur SD vers la sortie USB (Canal Gauche et Droite)
-AudioConnection          patchCord1(playWav1, 0, audioOutput, 0);
-AudioConnection          patchCord2(playWav1, 1, audioOutput, 1);
-
-// Utiliser le lecteur de carte SD intégré de la Teensy 3.6
+// Définition de la broche de la carte SD intégrée à la Teensy 3.6
 #define SDCARD_CS_PIN    BUILTIN_SDCARD
 
 void setup() {
@@ -21,7 +20,7 @@ void setup() {
   // Allouer de la mémoire pour le traitement audio
   AudioMemory(16);
 
-  // Initialisation de la carte SD intégrée
+  // Initialisation de la carte SD
   if (!(SD.begin(SDCARD_CS_PIN))) {
     while (1) {
       Serial.println("Erreur : Impossible de lire la carte SD !");
@@ -33,9 +32,14 @@ void setup() {
 }
 
 void loop() {
-  if (playWav1.isPlaying() == false) {
+  // Si le lecteur ne joue rien en ce moment...
+  if (playSdWav1.isPlaying() == false) {
     Serial.println("Lecture du fichier...");
-    playWav1.play("TRACK1.WAV"); 
-    delay(500); // On attend une demi-seconde pour laisser le temps au fichier de se lancer
+    
+    // Lance le fichier (Assure-toi qu'il s'appelle bien TRACK1.WAV sur ta SD)
+    playSdWav1.play("TRACK1.WAV"); 
+    
+    // Une petite pause pour laisser le temps au flux de démarrer
+    delay(500); 
   }
 }
